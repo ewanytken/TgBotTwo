ca9ce97e4e66a3f0b4da35e077be00dc849fae4e
ewanytken <ewanytken@mail.ru>
2022-03-22 21:15:12 +0300

    Restart from pengrad to telegram

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,27 +1,6 @@
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.Contact;
-import com.pengrad.telegrambot.model.Update;
 
-import java.util.List;
 
 public class Bot  {
 
-    private final TelegramBot bot = new TelegramBot("BOT_TOKEN");
-
-    public void server() {
-        bot.setUpdatesListener(updates -> {
-            updates.forEach(this::process);
-            return UpdatesListener.CONFIRMED_UPDATES_ALL;
-        });
-    }
-
-    private void process(Update update){
-        String caption = update.channelPost().caption();
-        System.out.println(caption);
-
-        Contact contact = update.message().contact();
-        System.out.println(contact);
-    }
 }
 
--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,5 +1,5 @@
 public class Starter {
     public static void main(String[] args){
-        new Bot();
+
     }
 }
